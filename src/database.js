f568cb1d9691d7f4bcea533ac52cfddb83b88b7e
Dario Kozicki <dariokozicki@gmail.com>
2023-04-12 10:28:29 -0300

    remove update once a day

--- src/database.js
+++ src/database.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-const consumer = require('./consumer/games.consumer.js')
+const mongoose = require("mongoose");
+const consumer = require("./consumer/games.consumer.js");
 const days = process.env.GAME_UPDATE_FREQUENCY_DAYS || 1;
 
-const URI = process.env.MONGODB_URI || 'mongodb://localhost/merntest';
+const URI = process.env.MONGODB_URI || "mongodb://localhost/merntest";
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-  console.log("Database is connected")
+connection.once("open", () => {
+  console.log("Database is connected");
   console.log("Setting interval for API requests every %d day(s)", days);
-  consumer.updateGames();
-  setInterval(consumer.updateGames, days * 1000 * 60 * 60 * 24)
+  // consumer.updateGames();
+  // setInterval(consumer.updateGames, days * 1000 * 60 * 60 * 24)
 });
